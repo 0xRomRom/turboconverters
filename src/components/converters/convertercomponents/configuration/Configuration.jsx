b2ea79f88f5d4ae298e69416575dcfe398450ba5
Roman - <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>
2023-09-23 18:25:47 +0200

    Save

--- src/components/converters/convertercomponents/configuration/Configuration.jsx
+++ src/components/converters/convertercomponents/configuration/Configuration.jsx
@@ -1,6 +1,14 @@
 import stl from "./Configuration.module.css";
 
 const Configuration = () => {
-  return <div className={stl.configurebox}></div>;
+  return (
+    <div className={stl.configurebox}>
+      <span className={stl.type}>Image</span>
+      <div className={stl.inputs}>
+        <span className={stl.convert}>Convert</span>
+        <span className={stl.input1}>JPG</span>
+      </div>
+    </div>
+  );
 };
 export default Configuration;
