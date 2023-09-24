6a34717756ec89d44a03aed339169d2a7215cd8b
0xRomRom <roman0811@live.nl>
2023-09-24 17:35:07 +0200

    Folder refactor

--- turboconverters/src/components/navbar/converters/ads/AdBlockLeft.jsx
+++
@@ -1,7 +0,0 @@
-import stl from "./AdBlockLeft.module.css";
-
-const AdBlockLeft = () => {
-  return <div className={stl.adblock}></div>;
-};
-
-export default AdBlockLeft;
--- turboconverters/src/components/navbar/converters/ads/AdBlockRight.jsx
+++
@@ -1,7 +0,0 @@
-import stl from "./AdBlockRight.module.css";
-
-const AdBlockRight = () => {
-  return <div className={stl.adblock}></div>;
-};
-
-export default AdBlockRight;
