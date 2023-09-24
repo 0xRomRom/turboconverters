0f7a22a72290a7039df4bc2d01dcc81ca728ee7b
Roman - <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>
2023-09-24 21:21:08 +0200

    File drag drop working

--- src/components/converters/convertercomponents/uploadbox/UploadBox.jsx
+++ src/components/converters/convertercomponents/uploadbox/UploadBox.jsx
@@ -1,15 +1,27 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 import stl from "./UploadBox.module.css";
 import logo from "../../../../assets/Fileplus.svg";
 
 const UploadBox = () => {
+  const [isDraggingOver, setIsDraggingOver] = useState(false);
+
   const handleClickDefault = (e) => {
     e.preventDefault();
   };
 
+  const handleDragOver = (e) => {
+    e.preventDefault(); // Prevent the default behavior of blocking file drops.
+    setIsDraggingOver(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDraggingOver(false);
+  };
+
   const onDrop = useCallback((acceptedFiles) => {
+    setIsDraggingOver(false);
     acceptedFiles.forEach((file) => {
       const reader = new FileReader();
 
@@ -26,17 +38,27 @@ const UploadBox = () => {
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
-    <div
-      className={stl.uploadbox}
-      {...getRootProps()}
-      onClick={handleClickDefault}
-    >
-      <button className={stl.uploadbtn} {...getRootProps()}>
-        <img src={logo} alt="Plus logo" className={stl.plus} /> Choose Files
-      </button>
-      <input {...getInputProps()} />
-      <span className={stl.dragdrop}>Drag & Drop</span>
-    </div>
+    <>
+      {isDraggingOver && (
+        <div className={stl.largedropbox} {...getRootProps()}>
+          <span>Drop Files</span>
+        </div>
+      )}
+
+      <div
+        className={stl.uploadbox}
+        {...getRootProps()}
+        onClick={handleClickDefault}
+        onDragOver={handleDragOver}
+        // onDragLeave={handleDragLeave}
+      >
+        <button className={stl.uploadbtn} {...getRootProps()}>
+          <img src={logo} alt="Plus logo" className={stl.plus} /> Choose Files
+        </button>
+        <input {...getInputProps()} />
+        <span className={stl.dragdrop}>Drag & Drop</span>
+      </div>
+    </>
   );
 };
 
