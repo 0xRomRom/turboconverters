17d30557c2a4142452f942b2d5963434a0510f1d
Roman - <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>
2023-10-09 20:01:24 +0200

    Contact us modal created

--- src/components/converters/convertercomponents/footer/Footer.jsx
+++ src/components/converters/convertercomponents/footer/Footer.jsx
@@ -2,12 +2,14 @@ import stl from "./Footer.module.css";
 import PrivacyPolicy from "../../../footercomponents/PrivacyPolicy";
 import Terms from "../../../footercomponents/Terms";
 import AboutUs from "../../../footercomponents/AboutUs";
+import ContactUs from "../../../footercomponents/ContactUs";
 import { useState } from "react";
 
 const Footer = () => {
   const [privacy, openPrivacy] = useState(false);
   const [terms, openTerms] = useState(false);
   const [aboutUs, openAboutus] = useState(false);
+  const [contactUs, openContactUs] = useState(false);
 
   const handleOpenPrivacy = () => {
     openPrivacy(!privacy);
@@ -21,9 +23,15 @@ const Footer = () => {
     openAboutus(!aboutUs);
   };
 
+  const handleOpenContactUs = () => {
+    openContactUs(!contactUs);
+  };
+
   return (
     <div className={stl.footer}>
-      <span className={stl.footspan}>Contact Us</span>
+      <span className={stl.footspan} onClick={handleOpenContactUs}>
+        Contact Us
+      </span>
       <span className={stl.footspan} onClick={handleOpenAboutUs}>
         About Us
       </span>
@@ -37,6 +45,7 @@ const Footer = () => {
       {privacy && <PrivacyPolicy closePrivacy={openPrivacy} />}
       {terms && <Terms closeTerms={openTerms} />}
       {aboutUs && <AboutUs closeAboutUs={handleOpenAboutUs} />}
+      {contactUs && <ContactUs closeContactUs={handleOpenContactUs} />}
     </div>
   );
 };
---
+++ src/components/footercomponents/ContactUs.jsx
@@ -0,0 +1,35 @@
+import stl from "./ContactUs.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import logo from "../../assets/Emailbanner.png";
+
+const ContactUs = (props) => {
+  return (
+    <div className={stl.wrapper}>
+      <div className={stl.modal}>
+        <div className={stl.closewrapper}>
+          <FontAwesomeIcon
+            icon={faXmark}
+            className={stl.close}
+            onClick={props.closeContactUs}
+          />
+        </div>
+        <img src={logo} className={stl.sitelogo} alt="Turbo Converters" />
+        <p className={stl.aboutusTxt}>
+          We're a group of software developers aiming to improve the file
+          conversion experience.
+          <br />
+          <br />
+          Difficult to navigate user interfaces along with conversion limits
+          typically provide a bad user experience.
+          <br />
+          <br />
+          Turbo Converters provides an intuitive application to waste no further
+          time converting your files!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
