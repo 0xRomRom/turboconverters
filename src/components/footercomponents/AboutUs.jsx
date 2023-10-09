2e6254ff392064777be5190f536495d1749fef11
Roman - <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>
2023-10-09 18:27:50 +0200

    About us modal created

--- src/components/footercomponents/AboutUs.jsx
+++ src/components/footercomponents/AboutUs.jsx
@@ -1,6 +1,7 @@
 import stl from "./AboutUs.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import logo from "../../assets/Logo.jpg";
 
 const AboutUs = (props) => {
   return (
@@ -13,7 +14,19 @@ const AboutUs = (props) => {
             onClick={props.closeAboutUs}
           />
         </div>
-        hi
+        <img src={logo} className={stl.sitelogo} alt="Turbo Converters" />
+        <p className={stl.aboutusTxt}>
+          We're a group of software developers aiming to improve the file
+          conversion experience.
+          <br />
+          <br />
+          Difficult to navigate user interfaces along with conversion limits
+          typically provide a bad user experience.
+          <br />
+          <br />
+          Turbo Converters provides an intuitive application to waste no further
+          time converting your files!
+        </p>
       </div>
     </div>
   );
